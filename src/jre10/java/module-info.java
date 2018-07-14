402e09ee5fff790a592cc5d451a0bb1566c88ba6
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:49:17 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,9 @@
+module com.jwebmp.plugins.bootswatch4 {
+	exports com.jwebmp.plugins.bootswatch4;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+}
