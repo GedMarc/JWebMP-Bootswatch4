82cdd9a7dc10c2dc09cf10880683fc49e435222a
GedMarc <ged_marc@hotmail.com>
2019-04-14 18:18:03 +0200

    DevKit and Structure with Cleanup

--- src/main/java/com/jwebmp/plugins/bs4/bootswatch/Bootswatch4Themes.java
+++ src/main/java/com/jwebmp/plugins/bs4/bootswatch/Bootswatch4Themes.java
@@ -30,27 +30,27 @@ import java.util.List;
 public enum Bootswatch4Themes
 		implements ITheme
 {
-	Cerulean("bootswatch4/Cerulean/bootstrap.min.css"),
-	Cosmo("bootswatch4/Cosmo/bootstrap.min.css"),
-	Cyborg("bootswatch4/Cyborg/bootstrap.min.css"),
-	Darkly("bootswatch4/Darkly/bootstrap.min.css"),
-	Flatly("bootswatch4/Flatly/bootstrap.min.css"),
-	Journal("bootswatch4/Journal/bootstrap.min.css"),
-	Litera("bootswatch4/Litera/bootstrap.min.css"),
-	lumen("bootswatch4/lumen/bootstrap.min.css"),
-	Lux("bootswatch4/Lux/bootstrap.min.css"),
-	Materia("bootswatch4/Materia/bootstrap.min.css"),
-	Minty("bootswatch4/Minty/bootstrap.min.css"),
-	Pulse("bootswatch4/Pulse/bootstrap.min.css"),
-	Sandstone("bootswatch4/Sandstone/bootstrap.min.css"),
-	Simplex("bootswatch4/Simplex/bootstrap.min.css"),
-	Sketchy("bootswatch4/Sketchy/bootstrap.min.css"),
-	Slate("bootswatch4/Slate/bootstrap.min.css"),
-	Solar("bootswatch4/Solar/bootstrap.min.css"),
-	Spacelab("bootswatch4/Spacelab/bootstrap.min.css"),
-	Superhero("bootswatch4/Superhero/bootstrap.min.css"),
-	United("bootswatch4/United/bootstrap.min.css"),
-	Yeti("bootswatch4/Yeti/bootstrap.min.css");
+	Cerulean("bower_components/bootswatch/dist/cerulean/bootstrap.min.css"),
+	Cosmo("bower_components/bootswatch/dist/cosmo/bootstrap.min.css"),
+	Cyborg("bower_components/bootswatch/dist/cyborg/bootstrap.min.css"),
+	Darkly("bower_components/bootswatch/dist/darkly/bootstrap.min.css"),
+	Flatly("bower_components/bootswatch/dist/flatly/bootstrap.min.css"),
+	Journal("bower_components/bootswatch/dist/journal/bootstrap.min.css"),
+	Litera("bower_components/bootswatch/dist/litera/bootstrap.min.css"),
+	lumen("bower_components/bootswatch/dist/lumen/bootstrap.min.css"),
+	Lux("bower_components/bootswatch/dist/lux/bootstrap.min.css"),
+	Materia("bower_components/bootswatch/dist/materia/bootstrap.min.css"),
+	Minty("bower_components/bootswatch/dist/minty/bootstrap.min.css"),
+	Pulse("bower_components/bootswatch/dist/pulse/bootstrap.min.css"),
+	Sandstone("bower_components/bootswatch/dist/sandstone/bootstrap.min.css"),
+	Simplex("bower_components/bootswatch/dist/simplex/bootstrap.min.css"),
+	Sketchy("bower_components/bootswatch/dist/sketchy/bootstrap.min.css"),
+	Slate("bower_components/bootswatch/dist/slate/bootstrap.min.css"),
+	Solar("bower_components/bootswatch/dist/solar/bootstrap.min.css"),
+	Spacelab("bower_components/bootswatch/dist/spacelab/bootstrap.min.css"),
+	Superhero("bower_components/bootswatch/dist/superhero/bootstrap.min.css"),
+	United("bower_components/bootswatch/dist/united/bootstrap.min.css"),
+	Yeti("bower_components/bootswatch/dist/yeti/bootstrap.min.css");
 
 	private String cssLocation;
 
