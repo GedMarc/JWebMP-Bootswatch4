8f5cf37bfee3518e9ec505a195e095e849ced67b
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:09 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,15 +2,15 @@ module com.jwebmp.plugins.bs4.bootswatch {
 	exports com.jwebmp.plugins.bs4.bootswatch;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.bs4.bootswatch.Bootswatch4PageConfigurator;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.bootswatch.implementations.Bootswatch4ExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.bootswatch.implementations.Bootswatch4ExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.bootswatch.implementations.Bootswatch4ExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.bootswatch.implementations.Bootswatch4ExclusionsModule;
 
 	opens com.jwebmp.plugins.bs4.bootswatch to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
