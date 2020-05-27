4541deae5f8b840783bae7536cfd2c6b814e7d89
GedMarc <ged_marc@hotmail.com>
2020-05-27 02:31:53 +0200

    Building after lint and removing cxf bundle for each dependency separately to keep in sync

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,7 +3,6 @@ module com.jwebmp.plugins.bs4.bootswatch {
 
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
-	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires com.guicedee.guicedinjection;
